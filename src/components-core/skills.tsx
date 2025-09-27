f783629aa7037993ce241a1a972f1f1bfe778840
Natan Araujo <natan.araujo.dev@gmail.com>
2025-09-26 23:26:14 -0300

    fix (skills): - skills agora é um grid (não quebra com o tamanho do site)

--- src/components-core/skills.tsx
+++ src/components-core/skills.tsx
@@ -23,8 +23,7 @@ export default function Skills() {
 
 			<div
 				className="
-				flex flex-row flex-wrap
-				justify-center items-stretch
+				grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3
 				gap-8"
 			>
 				{skillList.map((skill) => (
