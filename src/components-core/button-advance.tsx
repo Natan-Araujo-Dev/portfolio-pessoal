afcab5c98ac2713dc071631a3d5287d3b7ab04cf
Natan Araujo <natan.araujo.dev@gmail.com>
2025-09-29 13:27:45 -0300

    style (arrow): - arrow expande com o display

--- src/components-core/button-advance.tsx
+++ src/components-core/button-advance.tsx
@@ -7,9 +7,23 @@ interface ButtonAdvanceProps {
 export default function ButtonAdvance({ href }: ButtonAdvanceProps) {
 	return (
 		<div className="inline-flex animate-bounce [animation-duration:1.8s]">
-			<a href={href} className="expand-lg fill-color">
-				<ArrowDown size="40px" />
-			</a>
+			<div
+				className="
+				hidden lg:flex"
+			>
+				<a href={href} className="expand-lg fill-color">
+					<ArrowDown size="40px" />
+				</a>
+			</div>
+
+			<div
+				className="
+				lg:hidden"
+			>
+				<a href={href} className="expand-lg fill-color">
+					<ArrowDown size="30px" />
+				</a>
+			</div>
 		</div>
 	);
 }
